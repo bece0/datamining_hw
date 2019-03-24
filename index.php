f0b575a4a18994b7fba7c76755a6083bd087502e
bece0 <bgmclb45@gmail.com>
2019-03-24 23:41:34 +0300

    ilk commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Datamining</title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+        <style>
+            .container2{
+                margin-left: 50px;
+                margin-right: 50px;
+            }
+            .table-container{
+                overflow-x: auto;
+                white-space: nowrap;
+                height: 45vh;
+                wid
+            }
+        
+        
+        </style>
+    </head>
+
+</html>
